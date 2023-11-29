fe309f90e0adc304526efe37a74d77fb3517bcdb
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-11-29 19:28:49 +0900

    Delete unnecessary files and code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,9 @@
 // Assuming initializeFirebaseApp returns a Promise
 import type { AppProps } from "next/app";
-import { initializeFirebaseApp } from "@/lib/firebase/firebase";
-import { useEffect, useState } from "react";
-import { AuthProvider } from "@/feature/provider/AuthProvider";
+import "../styles/globals.css";
 
 function App({ Component, pageProps }: AppProps) {
-  const [isFirebaseInitialized, setFirebaseInitialized] = useState(false);
-
-  useEffect(() => {
-    const initializeApp = async () => {
-      try {
-        await initializeFirebaseApp();
-        setFirebaseInitialized(true);
-      } catch (error) {
-        console.error("Error initializing Firebase:", error);
-      }
-    };
-
-    initializeApp();
-  }, []);
-
-  if (!isFirebaseInitialized) {
-    // You can render a loading spinner or other UI while Firebase is initializing
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default App;
