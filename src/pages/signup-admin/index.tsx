8642f4a2b6b5159596cbbc8477eb786dd4b3b4f8
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-11-30 12:09:54 +0900

    Refactor signup-admin page and registration styles

--- src/pages/signup-admin/index.tsx
+++ src/pages/signup-admin/index.tsx
@@ -18,8 +18,6 @@ export default function RegisterAdmin() {
   const [streetAddress, setStreetAddress] = useState<string>("");
   const [phoneNumber, setPhoneNumber] = useState<string>("");
   const [email, setEmail] = useState<string>("");
-  const [additionalInfo, setAdditionalInfo] = useState<string>("");
-  const [selectedOption, setSelectedOption] = useState<string>(""); // Add this line
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const router = useRouter();
@@ -50,87 +48,110 @@ export default function RegisterAdmin() {
         <div className={style.wrapper}>
           <h1>会社情報</h1>
           <div className={style.content}>
-            <p>表示名</p>
-            <input
-              placeholder="株式会社〇〇"
-              type="text"
-              value={displayName}
-              onChange={(e) => setDisplayName(e.target.value)}
-            />
+            <div className={style.innerContent}>
+              <p>表示名</p>
+              <input
+                placeholder="株式会社〇〇"
+                type="text"
+                value={displayName}
+                onChange={(e) => setDisplayName(e.target.value)}
+              />
+            </div>
           </div>
           <div className={style.content}>
-            <p>会社ID</p>
-            <input
-              type="text"
-              placeholder="会社IDを入力してください"
-              value={companyId}
-              onChange={(e) => setCompanyId(e.target.value)}
-            />
+            <div className={style.innerContent}>
+              <p>会社ID</p>
+              <input
+                type="text"
+                placeholder="会社IDを入力してください"
+                value={companyId}
+                onChange={(e) => setCompanyId(e.target.value)}
+              />
+            </div>
           </div>
         </div>
         <div className={style.wrapper}>
           <h1>会社連絡先</h1>
           <div className={style.content}>
-            <p>代表者名</p>
-            <input
-              type="text"
-              placeholder="山田 太郎"
-              value={representativeName}
-              onChange={(e) => setRepresentativeName(e.target.value)}
-            />
-          </div>
-          <div className={style.address}>
-            <h2>住所</h2>
-            <div className={style.content}>
-              <p>郵便番号</p>
+            <div className={style.innerContent}>
+              <p>代表者名</p>
               <input
                 type="text"
-                placeholder="000-0000"
-                value={postalCode}
-                onChange={(e) => setPostalCode(e.target.value)}
+                placeholder="山田 太郎"
+                value={representativeName}
+                onChange={(e) => setRepresentativeName(e.target.value)}
               />
             </div>
+          </div>
+          <h1>住所</h1>
+          <div className={style.address}>
             <div className={style.content}>
-              <p>都道府県</p>
-              <input type="text" placeholder="大阪府" readOnly />
+              <div className={style.innerContent}>
+                <p>郵便番号</p>
+                <input
+                  type="text"
+                  placeholder="000-0000"
+                  value={postalCode}
+                  onChange={(e) => setPostalCode(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className={style.content}>
+              <div className={style.innerContent}>
+                <p>都道府県</p>
+                <input
+                  type="text"
+                  placeholder="大阪府"
+                  value={prefecture}
+                  onChange={(e) => setPrefecture(e.target.value)}
+                />
+              </div>
             </div>
           </div>
           <div className={style.content}>
-            <p>市区町村</p>
-            <input
-              type="text"
-              placeholder="北区中崎西"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-            />
+            <div className={style.innerContent}>
+              <p>市区町村</p>
+              <input
+                type="text"
+                placeholder="北区中崎西"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
+            </div>
           </div>
           <div className={style.content}>
-            <p>番地・建物名など</p>
-            <input
-              type="text"
-              value={streetAddress}
-              placeholder="2-3-15 ECCコンピュータ専門学校 2号館"
-              onChange={(e) => setStreetAddress(e.target.value)}
-            />
+            <div className={style.innerContent}>
+              <p>番地・建物名など</p>
+              <input
+                type="text"
+                value={streetAddress}
+                placeholder="2-3-15 ECCコンピュータ専門学校 2号館"
+                onChange={(e) => setStreetAddress(e.target.value)}
+              />
+            </div>
           </div>
           <div className={style.content}>
-            <p>電話番号</p>
-            <input
-              type="tel"
-              required
-              placeholder="06-1234-5678"
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-            />
+            <div className={style.innerContent}>
+              <p>電話番号</p>
+              <input
+                type="tel"
+                required
+                placeholder="06-1234-5678"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+            </div>
           </div>
           <div className={style.content}>
-            <p>メールアドレス</p>
-            <input
-              type="email"
-              value={email}
-              placeholder="company@example.com"
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            <div className={style.innerContent}>
+              <p>メールアドレス</p>
+              <input
+                type="email"
+                value={email}
+                placeholder="company@example.com"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
           </div>
         </div>
         <div className={style.submitWrap}>
