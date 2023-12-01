95dcd7ac0a4ade02b2f32120d408594fba46155d
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-12-01 13:40:47 +0900

    Add react-icons package to package.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import dayjs from "dayjs";
+import "dayjs/locale/ja";
 import { useEffect } from "react";
+import style from "@/styles/attendance.module.scss";
+import {
+  FaRightToBracket,
+  FaRightFromBracket,
+  FaCircleStop,
+  FaCirclePlay,
+  FaRegClock,
+} from "react-icons/fa6";
 
 const AttendanceChecker = () => {
-  const [currentTime, setCurrentTime] = useState(dayjs());
+  const [currentTime, setCurrentTime] = useState<dayjs.Dayjs | null>(null);
   const [startWork, setStartWork] = useState<dayjs.Dayjs | null>(null);
   const [startBreak, setStartBreak] = useState<dayjs.Dayjs | null>(null);
   const [endBreak, setEndBreak] = useState<dayjs.Dayjs | null>(null);
@@ -50,25 +59,71 @@ const AttendanceChecker = () => {
     return 0;
   };
 
+  const getAttendanceState = () => {
+    if (startWork && !endWork) {
+      if (isOnBreak) {
+        return { icon: <FaCircleStop />, text: "休憩中", class: style.break };
+      } else {
+        return { icon: <FaRegClock />, text: "出勤中", class: style.start };
+      }
+    } else if (endWork) {
+      return {
+        icon: <FaRightFromBracket />,
+        text: "退勤済み",
+        class: style.end,
+      };
+    }
+    return { icon: <FaRightToBracket />, text: "未出勤", class: style.end };
+  };
+
+  const { icon, text, class: attendanceClass } = getAttendanceState();
+
   return (
-    <div>
-      <h2>{currentTime.format("YYYY年MM月DD日(dd)")}</h2>
-      <p>{currentTime.format("HH:mm:ss")}</p>
-      <button disabled={startWork !== null} onClick={handleStartWork}>
-        出勤
-      </button>
-      <button
-        disabled={startWork === null || endWork !== null}
-        onClick={isOnBreak ? handleEndBreak : handleStartBreak}
-      >
-        {isOnBreak ? "休憩終了" : "休憩開始"}
-      </button>
-      <button
-        disabled={startWork === null || endWork !== null || isOnBreak}
-        onClick={handleEndWork}
-      >
-        退勤
-      </button>
+    <div className={style.body}>
+      <div className={`${style.currentState} ${attendanceClass}`}>
+        <div className={style.iconState}>{icon}</div>
+        <div>
+          <p className={style.year}>
+            {currentTime ? currentTime.format("YYYY年MM月DD日(dd)") : ""}
+          </p>
+          <div className={style.timeWrap}>
+            <p className={style.time}>
+              {currentTime ? currentTime.format("HH:mm:ss") : ""}
+            </p>
+            <p className={style.timeState}>({text})</p>
+          </div>
+        </div>
+      </div>
+      <div className={style.btnWrap}>
+        <button
+          className={`${style.button} ${startWork ? style.start : style.start}`}
+          disabled={startWork !== null}
+          onClick={handleStartWork}
+        >
+          <span>
+            <FaRightToBracket />
+          </span>
+          出勤
+        </button>
+        <button
+          className={`${style.button} ${isOnBreak ? style.break : style.break}`}
+          disabled={startWork === null || endWork !== null}
+          onClick={isOnBreak ? handleEndBreak : handleStartBreak}
+        >
+          <span>{isOnBreak ? <FaCircleStop /> : <FaCirclePlay />} </span>
+          {isOnBreak ? "休憩終了" : "休憩開始"}
+        </button>
+        <button
+          className={`${style.button} ${endWork ? style.end : style.end}`}
+          disabled={startWork === null || endWork !== null || isOnBreak}
+          onClick={handleEndWork}
+        >
+          <span>
+            <FaRightFromBracket />
+          </span>
+          退勤
+        </button>
+      </div>
       <p>Worked Hours: {calculateWorkHours()}</p>
     </div>
   );
