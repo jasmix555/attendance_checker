e4d578b35ede81e4397f2bb13a198508c7d03633
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-12-02 12:20:51 +0900

    Add motion styles and registration functionality

--- src/components/Motion.tsx
+++ src/components/Motion.tsx
@@ -8,6 +8,13 @@ export default function Motion({ children }: { children: React.ReactNode }) {
           initial={{ opacity: 0, transform: "translateX(100)" }} //初期状態
           animate={{ opacity: 1, transform: "translateX(0)" }} //マウント
           exit={{ opacity: 0, transform: "translateX(100)" }} //アンマウント
+          style={{
+            height: "100vh",
+            width: "100vw",
+            backgroundColor: "#fff",
+            overflow: "hidden",
+            position: "relative",
+          }}
         >
           {children}
         </motion.main>
--- src/pages/signup-admin/index.tsx
+++ src/pages/signup-admin/index.tsx
@@ -10,6 +10,7 @@ import style from "@/styles/input.module.scss";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import Link from "next/link";
 import Layout from "@/components/Layout";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 export default function Register() {
   const [email, setEmail] = useState<string>("");
@@ -27,13 +28,22 @@ export default function Register() {
 
     try {
       const auth = getAuth();
+      const db = getFirestore();
+
       const userCredential: UserCredential =
         await createUserWithEmailAndPassword(auth, email, password);
       await sendEmailVerification(userCredential.user);
-      // Registration successful, redirect to the profile setup page
+
+      // Create a new document in the "admin" collection with the user's ID
+      await setDoc(doc(db, "admin", userCredential.user.uid), {
+        // Add any initial data you want to store for the admin here
+      });
+
+      // Redirect all users to the "/register-company" page after registration
+      router.push("/register-company");
+
       setEmail("");
       setPassword("");
-      router.push("/signin");
     } catch (error) {
       console.error(error);
     } finally {
@@ -43,49 +53,45 @@ export default function Register() {
 
   return (
     <Layout>
-      <div className={style.bodyWrap}>
-        <form onSubmit={handleSubmit}>
-          <div className={style.contentWrap}>
-            <div className={style.inputWrap}>
-              <p>E-Mail</p>
-              <input
-                autoComplete="off"
-                className={style.input}
-                type="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className={style.inputWrap}>
-              <p>Password</p>
-              <div className={style.iconVis}>
-                <input
-                  autoComplete="new-password"
-                  className={style.input}
-                  type={show ? "text" : "password"}
-                  name="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-                <i onClick={handleClick}>
-                  {show ? <AiFillEyeInvisible /> : <AiFillEye />}
-                </i>
-              </div>
-            </div>
-            <div className={style.submitWrap}>
-              <button type="submit" disabled={isLoading}>
-                {isLoading ? "作成中" : "新規登録"}
-              </button>
-              <div>
-                <Link href={"/welcome"}>戻る</Link>
-              </div>
-            </div>
+      <form onSubmit={handleSubmit} className={style.form} autoComplete="off">
+        <div className={style.wrapper}>
+          <h2>新規登録</h2>
+
+          <div className={style.inputWrapper}>
+            <input
+              autoComplete="off"
+              className={style.input}
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label htmlFor="email">メール</label>
           </div>
-        </form>
-      </div>
+          <div className={style.inputWrapper}>
+            <input
+              autoComplete="new-password"
+              className={style.input}
+              type={show ? "text" : "password"}
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <label htmlFor="password">パスワード</label>
+            <i onClick={handleClick}>
+              {show ? <AiFillEyeInvisible /> : <AiFillEye />}
+            </i>
+          </div>
+          <div className={style.submitWrap}>
+            <button type="submit" disabled={isLoading}>
+              {isLoading ? "作成中" : "作成"}
+            </button>
+            <Link href={"/welcome"}>戻る</Link>
+          </div>
+        </div>
+      </form>
     </Layout>
   );
 }
